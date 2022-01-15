14de70d335d352d6130292ec3cafccf630403572
YSJ <sungzzuu@gmail.com>
2022-01-15 12:22:20 +0900

    플레이어 회전 구현 중 & 카메라 회전 안하도록 & 3주차 보고서 작성완료

--- Client_Desert/Player.cpp
+++ Client_Desert/Player.cpp
@@ -64,43 +64,54 @@ void CPlayer::Move(DWORD dwDirection, float fDistance, bool bUpdateVelocity)
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, fDistance * (1 / sqrtf(2)));
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, fDistance*(1/sqrtf(2)));
+		//RotateMesh(XMFLOAT3(0.f, 45.f, 0.f));
 	}
 	else if (CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_D) &&
 		CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_S))	// ¿À¾Æ
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, -fDistance * (1 / sqrtf(2)));
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, fDistance * (1 / sqrtf(2)));
+		//RotateMesh(XMFLOAT3(0.f, 135.f, 0.f));
+
 	}
 	else if (CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_S) &&
 		CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_A))	// ¾Æ¿Þ
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, -fDistance * (1 / sqrtf(2)));
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, -fDistance * (1 / sqrtf(2)));
+		//RotateMesh(XMFLOAT3(0.f, 225.f, 0.f));
+
 	}
 	else if (CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_A) &&
 		CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_W))	// ¿ÞÀ§
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, fDistance * (1 / sqrtf(2)));
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, -fDistance * (1 / sqrtf(2)));
+		//RotateMesh(XMFLOAT3(0.f, 315.f, 0.f));
+
 	}
 	else if (CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_D))
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, fDistance);
+		//RotateMesh(XMFLOAT3(0.f, 90.f, 0.f));
 
 	}
 	else if (CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_A))
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Right, -fDistance);
+		//RotateMesh(XMFLOAT3(0.f, 270.f, 0.f));
 
 	}
 	else if (CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_W))
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, fDistance);
+		//RotateMesh(XMFLOAT3(0.f, 0.f, 0.f));
 
 	}
 	else if (CInputDev::GetInstance()->KeyPressing(DIKEYBOARD_S))
 	{
 		xmf3Shift = Vector3::Add(xmf3Shift, m_xmf3Look, -fDistance);
+		//RotateMesh(XMFLOAT3(0.f, 180.f, 0.f));
 
 	}
 	else
@@ -201,9 +212,9 @@ void CPlayer::Update(float fTimeElapsed)
 	if (m_pPlayerUpdatedContext) OnPlayerUpdateCallback(fTimeElapsed);
 
 	DWORD nCurrentCameraMode = m_pCamera->GetMode();
-	if (nCurrentCameraMode == THIRD_PERSON_CAMERA) m_pCamera->Update(m_xmf3Position, fTimeElapsed);
-	if (m_pCameraUpdatedContext) OnCameraUpdateCallback(fTimeElapsed);
-	if (nCurrentCameraMode == THIRD_PERSON_CAMERA) m_pCamera->SetLookAt(m_xmf3Position);
+	//if (nCurrentCameraMode == THIRD_PERSON_CAMERA) m_pCamera->Update(m_xmf3Position, fTimeElapsed);
+	//if (m_pCameraUpdatedContext) OnCameraUpdateCallback(fTimeElapsed);
+	//if (nCurrentCameraMode == THIRD_PERSON_CAMERA) m_pCamera->SetLookAt(m_xmf3Position);
 	m_pCamera->RegenerateViewMatrix();
 
 	fLength = Vector3::Length(m_xmf3Velocity);
@@ -270,6 +281,38 @@ void CPlayer::Render(ID3D12GraphicsCommandList *pd3dCommandList, CCamera *pCamer
 {
 	DWORD nCameraMode = (pCamera) ? pCamera->GetMode() : 0x00;
 	if (nCameraMode == THIRD_PERSON_CAMERA) CGameObject::Render(pd3dCommandList, pCamera);
+}
+
+void CPlayer::RotateMesh(XMFLOAT3 xmf3Rotate)
+{
+	if (xmf3Rotate.x != 0.0f)
+	{
+		m_fPitch = xmf3Rotate.x;
+		if (m_fPitch > +89.0f) { xmf3Rotate.x -= (m_fPitch - 89.0f); m_fPitch = +89.0f; }
+		if (m_fPitch < -89.0f) { xmf3Rotate.x -= (m_fPitch + 89.0f); m_fPitch = -89.0f; }
+	}
+	if (xmf3Rotate.y != 0.0f)
+	{
+		if (int(m_fYaw) == int(xmf3Rotate.y))
+			return;
+
+		m_fYaw = xmf3Rotate.y;
+		if (m_fYaw > 360.0f) m_fYaw -= 360.0f;
+		if (m_fYaw < 0.0f) m_fYaw += 360.0f;
+	}
+	if (xmf3Rotate.z != 0.0f)
+	{
+		m_fRoll += xmf3Rotate.z;
+		if (m_fRoll > +20.0f) { xmf3Rotate.z -= (m_fRoll - 20.0f); m_fRoll = +20.0f; }
+		if (m_fRoll < -20.0f) { xmf3Rotate.z -= (m_fRoll + 20.0f); m_fRoll = -20.0f; }
+	}
+	if (xmf3Rotate.y != 0.0f)
+	{
+		XMMATRIX xmmtxRotate = XMMatrixRotationAxis(XMLoadFloat3(&m_xmf3Up), XMConvertToRadians(xmf3Rotate.y));
+		m_xmf3Look = Vector3::TransformNormal(m_xmf3Look, xmmtxRotate);
+		m_xmf3Right = Vector3::TransformNormal(m_xmf3Right, xmmtxRotate);
+	}
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
