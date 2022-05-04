e31b0702f03ae48c6d03e529fe66256b7aeedebe
YSJ <sungzzuu@gmail.com>
2022-05-05 02:26:57 +0900

    nan 오류 디버그 중

--- Client_Desert/Player.cpp
+++ Client_Desert/Player.cpp
@@ -330,20 +330,6 @@ void CPlayer::Update(float fTimeElapsed)
 
 	m_pCamera->Update(GetLook(), fTimeElapsed);
 
-	////////////////////////////////////////////////////////////////////////
-	m_xmf3Velocity = Vector3::Add(m_xmf3Velocity, m_xmf3Gravity);
-	float fLength = sqrtf(m_xmf3Velocity.x * m_xmf3Velocity.x + m_xmf3Velocity.z * m_xmf3Velocity.z);
-	float fMaxVelocityXZ = m_fMaxVelocityXZ;
-	if (fLength > m_fMaxVelocityXZ)
-	{
-		//m_xmf3Velocity.x *= (fMaxVelocityXZ / fLength);
-		//m_xmf3Velocity.z *= (fMaxVelocityXZ / fLength);
-	}
-	float fMaxVelocityY = m_fMaxVelocityY;
-	fLength = sqrtf(m_xmf3Velocity.y * m_xmf3Velocity.y);
-	if (fLength > m_fMaxVelocityY) m_xmf3Velocity.y *= (fMaxVelocityY / fLength);
-
-	//XMFLOAT3 xmf3Velocity = Vector3::ScalarProduct(m_xmf3Velocity, fTimeElapsed, false);
 	Move(m_xmf3Velocity, false);
 
 	if (m_pPlayerUpdatedContext) OnPlayerUpdateCallback(fTimeElapsed);
@@ -354,11 +340,7 @@ void CPlayer::Update(float fTimeElapsed)
 	if (nCurrentCameraMode == THIRD_PERSON_CAMERA) m_pCamera->SetLookAt(m_xmf3Position);
 	m_pCamera->RegenerateViewMatrix();
 
-	fLength = Vector3::Length(m_xmf3Velocity);
-	float fDeceleration = (m_fFriction * fTimeElapsed);
-	if (fDeceleration > fLength) fDeceleration = fLength;
-	m_xmf3Velocity = Vector3::Add(m_xmf3Velocity, Vector3::ScalarProduct(m_xmf3Velocity, -fDeceleration, true));
-
+	m_xmf3Velocity = {0.00f,0.00f ,0.00f };
 	LerpRotate(fTimeElapsed);
 
 	Blending_Animation(fTimeElapsed);
